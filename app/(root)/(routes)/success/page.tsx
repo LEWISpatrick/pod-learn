549604e1cc267d522d75beff52887c1172fb2972
LEWISpatrick <133691910+LEWISpatrick@users.noreply.github.com>
2024-10-11 11:18:13 -0300

    (chore) added emoji

--- app/(root)/(routes)/success/page.tsx
+++ app/(root)/(routes)/success/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function SuccessPage() {
   return (
     <div className="max-w-2xl mx-auto mt-10 p-6 rounded-lg shadow-md text-center">
-      <h1 className="text-3xl font-bold mb-4">Payment Successful!</h1>
+      <h1 className="text-3xl font-bold mb-4">Payment Successful 🙂!</h1>
       <p className="mb-6">
         Thank you for upgrading to premium. You now have unlimited podcast
         creation!
